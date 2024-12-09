d437b5211dd5842d4c23fe6bcb9191d95b51d607
dantekadagi3 <dantekadagi3@gmail.com>
2024-12-09 22:57:46 +0300

    updates to frontend

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -1,12 +1,16 @@
+
+import '../Styles/banner.css'
 function Banner(){
 
     return(
     <div className="text--button">
-        <h3>
+        <div>
+        <p className='main--text'>
             I&apos;m available<br/>
             For freelance projects
-        </h3>
-        <p>Tell me your idea, and I will code it to reality</p>
+        </p>
+        <p className='primary--text'>Tell me your idea, and I will code it to reality</p>
+     </div>
         <button>Hire Me</button>
     </div>
     )
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,45 @@
+import Contactinfo from '../components/Contactinfo';
+import '../Styles/contact.css';
+import phone from '../assets/phone.png';
+import mail from '../assets/mail.png';
+
+function Contact() {
+  return (
+    <section className="contact-section">
+      <div>
+        <h1>Contact Me</h1>
+        <hr />
+      </div>
+      <div className="left">
+        <Contactinfo
+          icon={phone}
+          text1="+254757700920"
+          text2="+254720403002"
+          className="information"
+        />
+        <Contactinfo
+          icon={mail}
+          text1="dantekadagi3@gmail.com"
+          text2="kadagidante3@gmail.com"
+          className="information"
+        />
+      </div>
+
+      <div className="right">
+        <form>
+          <div className="top">
+            <input type="text" placeholder="Your name" />
+            <input type="email" placeholder="Your email" />
+          </div>
+
+          <div className="bottom">
+            <textarea placeholder="Your message"></textarea>
+          </div>
+          <button type="submit">Send Message</button>
+        </form>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
